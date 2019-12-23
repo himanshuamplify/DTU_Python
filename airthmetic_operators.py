608acbe5e79c3211fa1c00ec71db6bb879482c9e
himanshuamplify <himanshusharma483@gmail.com>
2019-12-23 11:15:04 +0530

    Add files via upload

---
+++ airthmetic_operators.py
@@ -0,0 +1,50 @@
+#Airthmetic operators
+##a=100
+##b=20
+##print("a+b", a+b)
+##print("a-b", a-b)
+##print("a*b", a*b)
+##print("a/b", a/b)
+##print("a%b", a%b)
+##print("a**b", a**b)
+##print("a//b",a//b)
+
+#Comparison Operator
+a=10
+b=20
+print("a>b", a>b)
+print("a<b", a<b)
+print("a>=b", a>=b)
+print("a<=b", a<=b)
+print("a==b", a==b)
+
+#taking string value
+a='ravi'
+b='Chw'
+print("a>b", a>b)
+print("a<b", a<b)
+print("a>=b", a>=b)
+print("a<=b", a<=b)
+print("a==b", a==b)
+
+
+a=True   #1
+b=False  #0
+print("a>b", a>b)
+print("a<b", a<b)
+print("a>=b", a>=b)
+print("a<=b", a<=b)
+print("a==b", a==b)
+
+
+a=[1,3,5,90,10]
+b=20
+print( a is b)    #False
+a=10
+b=10
+print(a is b)
+
+##bitwise operator
+print(10<<2)==>40 ==>001010
+>> operator
+(10>>2)==>
